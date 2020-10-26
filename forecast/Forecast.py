2d7a733cad862b9586df6ee1cb375039d9653b13
김윤지 <yunjiyunji1@naver.com>
2020-10-26 15:37:30 +0900

    program works.. Complete Refactoring,,

--- forecast/Forecast.py
+++ forecast/Forecast.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from keras.layers import Dense
 from keras.callbacks import EarlyStopping
 
@@ -7,28 +7,19 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 
-# 데이터 가공하기
-
-# # 기온,강수량,풍속,습도,이슬점,현지기압,시정
-#
-# # 습도 = 습도 * 0.01
-# # 기압 = 기압 - 1000
-# # 시정 = 시정 * 0.00001
-
 #########################################################################
 #  Load DNN Model
 #########################################################################
 def load_model_except():
-    model = input("Enter Model Name : ");
+    m = input("Enter Model Name : ");
 
     try:
-        Weather_Forecast_Mode_Local = load_model(model);
+        Weather_Forecast_Mode_Local = load_model(m);
     except:
         print("Load Fail\n");
         Weather_Forecast_Mode_Local = -1;
 
     return Weather_Forecast_Mode_Local;
-
 
 Weather_Forecast_Model = load_model_except();
 
@@ -44,12 +35,12 @@ print("\n\nLoad Model\n\n");
 #  Load Current Weather Data
 #########################################################################
 def load_Data_except():
-    curr = input("Input Current Weather Data : ");
+    local = input("Input Current Weather Data : ");
 
     try:
-        X_data_Local = pd.read_csv(curr, names=['기온', '강수량', '풍속', '습도', '이슬점', '기압', '시정']);
+        X_data_Local = pd.read_csv(local, names=['기온', '습도', '이슬점', '기압']);
     except:
-        print("파일열기 실패\n");
+        print("Load Fail\n");
         X_data_Local = None;
 
     return X_data_Local;
@@ -59,72 +50,69 @@ X_data = load_Data_except();
 while(X_data is None):
     X_data = load_Data_except();
 
-print("불러오기 완료\n\n");
+print("Load Local Data\n\n");
 
+#########################################################################
+#  Get time to predict
+#########################################################################
 
-# 앞으로의 예측시간
-Time_Hour = input("예측시간: ");
-Time_Hour = int(Time_Hour);
+pred_time = input("Predict Time (hour) : ");
+pred_time = int(pred_time);
 
-# 데이터 가공
-#X_data.iloc[0,0] = X_data.iloc[0,0] * 0.1;
-X_data.iloc[0, 3] = X_data.iloc[0, 3] * 0.01;
-#X_data.iloc[0,4] = X_data.iloc[0,4] * 0.1;
-X_data.iloc[0, 5] = (X_data.iloc[0, 5] - 1000);
-X_data.iloc[0, 6] = X_data.iloc[0, 6] * 0.01;
+#########################################################################
+#  Data Processing
+#########################################################################
 
+# 0 Temperature
 
-print("입력 데이터");
+# 1 Humidity        습도 = 습도 * 0.01
+X_data.iloc[0, 1] = X_data.iloc[0, 1] * 0.01;
+
+# 2 Dew Point
+
+# 3 Pressure        기압 = 기압 - 1000
+X_data.iloc[0, 3] = (X_data.iloc[0, 3] - 1000);
+
+print("Current Local Weather");
 print(X_data);
 print("\n\n\n");
 
-Temp = Weather_Forecast_Model.predict(X_data);
+#########################################################################
+#  Predict
+#########################################################################
+prediction = Weather_Forecast_Model.predict(X_data);
 
 time = 0;
-while(time < Time_Hour - 1):
-    Temp = Weather_Forecast_Model.predict(Temp);
+while(time < pred_time - 1):
+    Temp = Weather_Forecast_Model.predict(prediction);
     time = time + 1;
 
+# result data processing
+Forecast_Temperature = prediction[0][0];
+Forecast_Humidity = prediction[0][1] * 100;
+Forecast_Dew_Point = prediction[0][2];
+Forecast_Pressure = (prediction[0][3]) + 1000;
 
-# 데이터 가공 및 결과 받아오기
-Forecast_Temperature = Temp[0][0];
+#########################################################################
+#  Print and Save Result
+#########################################################################
+print(">>>>>> Weather Forecast <<<<<<\n\n");
 
-# 1미만이면 그냥 없는 것으로 하자 0으로 나눔 예외발생한다.
-if Temp[0][1] < 1:
-    Forecast_Rain = 0;
-else:
-    Forecast_Rain = Temp[0][1];
-
-Forecast_Wind = Temp[0][2];
-Forecast_Humidity = Temp[0][3] * 100;
-Forecast_Dew_Point = Temp[0][4];
-Forecast_Pressure = (Temp[0][5]) + 1000;
-Forecast_Sight = Temp[0][6] * 1000;
-
-
-print("기상예보 \n\n");
-
-print("예보 데이터");
-print(Temp);
+print("Local Weather after ", pred_time, " Hour(s)");
+print(prediction);
 print("\n");
 
 print("기온: ", Forecast_Temperature, "C");
-print("강수량: ", Forecast_Rain, "mm");
-print("풍속: ", Forecast_Wind, "M/s");
 print("습도: ", Forecast_Humidity, "%");
-
 print("이슬점: ", Forecast_Dew_Point, "C");
 print("기압: ", Forecast_Pressure, "hpa");
-print("시정: ", Forecast_Sight, "Meter");
 
-answear = input("\n\n결과 저장하기 >");
+# save
+out = input("\n\nSave Prediction >");
 
-Save_Array = [Forecast_Temperature,Forecast_Rain,Forecast_Wind,
-              Forecast_Humidity,Forecast_Dew_Point,Forecast_Pressure,
-              Forecast_Sight];
-
-Save_File = [ Save_Array];
-
+Save_Array = [Forecast_Temperature, Forecast_Humidity, Forecast_Dew_Point, Forecast_Pressure];
+Save_File = [Save_Array];
 Forecast_File = pd.DataFrame(Save_File);
-Forecast_File.to_csv(answear, header = False, index = False);
-print("\n\n 결과저장완료");
+Forecast_File.to_csv(out, header = False, index = False);
+
+print("\n\n SAVE \n");
